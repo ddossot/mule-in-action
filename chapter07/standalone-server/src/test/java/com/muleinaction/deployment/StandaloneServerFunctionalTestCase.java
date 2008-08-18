3308ac17b08c0b5bd811f4afed1e581fc5dcc0c1
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2008-08-18 18:31:40 +0000

    Added Jetty-based standalone server sample

---
+++ chapter07/standalone-server/src/test/java/com/muleinaction/deployment/StandaloneServerFunctionalTestCase.java
@@ -0,0 +1,24 @@
+package com.muleinaction;
+
+import org.mule.api.service.Service;
+import org.mule.tck.FunctionalTestCase;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class StandaloneServerFunctionalTestCase extends FunctionalTestCase {
+
+	@Override
+	protected String getConfigResources() {
+		return "conf/standalone-server-config.xml";
+	}
+
+	public void testCorrectlyInitialized() throws Exception {
+		final Service service = muleContext.getRegistry().lookupService(
+				"RandomIntegerGenerator");
+
+		assertNotNull(service);
+		assertEquals("StandaloneServer", service.getModel().getName());
+	}
+
+}
