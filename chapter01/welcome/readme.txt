852c16530baacfa44f28df36f363aeec6a9c8c7e
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2008-09-02 23:17:08 +0000

    Added welcome example for chapter 1

---
+++ chapter01/welcome/readme.txt
@@ -0,0 +1,7 @@
+Welcome to Mule!
+----------------
+
+Start this example with the script that is relevant for your OS.
+
+After being greeted with an ASCII-art message, press control-C to stop Mule.
+
---
+++ chapter01/welcome/src/test/java/com/muleinaction/WelcomeFunctionalTestCase.java
@@ -0,0 +1,24 @@
+package com.muleinaction;
+
+import org.mule.api.service.Service;
+import org.mule.tck.FunctionalTestCase;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class WelcomeFunctionalTestCase extends FunctionalTestCase {
+
+	@Override
+	protected String getConfigResources() {
+		return "conf/welcome-config.xml";
+	}
+
+	public void testCorrectlyInitialized() throws Exception {
+		final Service service = muleContext.getRegistry().lookupService(
+				"welcomeService");
+
+		assertNotNull(service);
+		assertEquals("welcomeModel", service.getModel().getName());
+	}
+
+}
