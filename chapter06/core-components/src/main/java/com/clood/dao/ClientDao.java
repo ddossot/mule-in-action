e34fcd90949b42d89ec9830d2d84a3511fafffbd
ddossot <david@dossot.net>
2008-09-09 07:09:59 +0000

    Refactored builder example to be more demonstrative

---
+++ chapter06/core-components/src/main/java/com/clood/dao/ClientDao.java
@@ -0,0 +1,17 @@
+package com.clood.dao;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+import com.clood.model.Client;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class ClientDao {
+
+	public Client findById(final Long id) {
+		return new Client(id, RandomStringUtils.randomAscii(5), RandomStringUtils
+				.randomAscii(10));
+	}
+
+}
---
+++ chapter06/core-components/src/main/java/com/clood/statistic/ActivityEmailContext.java
@@ -0,0 +1,45 @@
+package com.clood.statistic;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+import com.clood.model.Client;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class ActivityEmailContext {
+	private final long id;
+	private Client client;
+	private ActivityReport activityReport;
+
+	public ActivityEmailContext(final long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(final Client client) {
+		this.client = client;
+	}
+
+	public ActivityReport getActivityReport() {
+		return activityReport;
+	}
+
+	public void setActivityReport(final ActivityReport activityReport) {
+		this.activityReport = activityReport;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this,
+				ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+}
