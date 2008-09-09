5faa35778f2811378171eafb5515fa4c793a0fe6
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2008-09-09 11:12:00 +0000

    http transport unit tests are passing

--- chapter03/http/src/test/java/com/muleinaction/HttpOutboundFunctionalTestCase.java
+++ chapter03/http/src/test/java/com/muleinaction/HttpOutboundFunctionalTestCase.java
@@ -6,19 +6,19 @@ import org.mule.tck.FunctionalTestCase;
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
-public class HttpTransportFunctionalTestCase extends FunctionalTestCase {
+public class HttpOutboundFunctionalTestCase extends FunctionalTestCase {
 
 	@Override
 	protected String getConfigResources() {
-		return "conf/http-config.xml";
+		return "conf/http-outbound-config.xml";
 	}
 
 	public void testCorrectlyInitialized() throws Exception {
 		final Service service = muleContext.getRegistry().lookupService(
-				"httpService");
+				"httpOutboundService");
 
 		assertNotNull(service);
-		assertEquals("httpModel", service.getModel().getName());
+		assertEquals("httpOutboundModel", service.getModel().getName());
 	}
 
 }
