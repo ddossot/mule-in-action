477472c9a37c0bfccf103a6803098fb34f55ff16
ddossot <david@dossot.net>
2008-10-04 21:44:30 +0000

    Added demo of raw usage of MuleClient

---
+++ chapter13/mule-client/src/test/java/com/muleinaction/muleclient/RawMuleClientTest.java
@@ -0,0 +1,26 @@
+package com.muleinaction.muleclient;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class RawMuleClientTest {
+    @Test
+    public void tapHttpTransport() throws Exception {
+        final MuleClient muleClient = new MuleClient();
+        final MuleMessage response = muleClient
+                .send(
+                        "http://finance.google.com/finance/historical?q=GOOG&histperiod=weekly&output=csv",
+                        null, null);
+
+        assertNotNull(response);
+        assertTrue(response.getPayloadAsString().contains(
+                "Date,Open,High,Low,Close,Volume"));
+    }
+}
