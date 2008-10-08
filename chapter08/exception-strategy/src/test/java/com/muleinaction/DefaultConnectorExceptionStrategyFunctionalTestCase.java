837825db5a138bba5de17a181da8c3ea98b254c2
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2008-10-08 21:43:11 +0000

    more tests for ch08

---
+++ chapter08/exception-strategy/src/test/java/com/muleinaction/DefaultConnectorExceptionStrategyFunctionalTestCase.java
@@ -0,0 +1,24 @@
+package com.muleinaction;
+
+import org.mule.api.service.Service;
+import org.mule.tck.FunctionalTestCase;
+
+/**
+ * @author John D'Emic (john.demic@gmail.com)
+ */
+public class DefaultConnectorExceptionStrategyFunctionalTestCase extends FunctionalTestCase {
+
+	@Override
+	protected String getConfigResources() {
+		return "conf/default-connector-exception-strategy-config.xml";
+	}
+
+	public void testCorrectlyInitialized() throws Exception {
+		final Service service = muleContext.getRegistry().lookupService(
+				"defaultConnectorExceptionStrategyService");
+
+		assertNotNull(service);
+		assertEquals("defaultConnectorExceptionStrategyModel", service.getModel().getName());
+	}
+
+}
