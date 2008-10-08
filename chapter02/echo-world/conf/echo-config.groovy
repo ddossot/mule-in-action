ec835d6326a453e0b88e418a6e0565e141bb18d8
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2008-10-08 07:20:48 +0000

    Migrated to Mule 2.1

--- chapter02/echo-world/conf/echo-config.groovy
+++ chapter02/echo-world/conf/echo-config.groovy
@@ -11,28 +11,30 @@ import org.mule.api.service.*
 import org.mule.routing.inbound.*
 import org.mule.routing.outbound.*
 import org.mule.model.seda.*
+import org.mule.retry.policies.*
 
 // Register required system objects
-queueManager = new TransactionalQueueManager();
+queueManager = new TransactionalQueueManager()
 queueManager.persistenceStrategy = new MemoryPersistenceStrategy()
-muleContext.registry.registerObject(MuleProperties.OBJECT_QUEUE_MANAGER, queueManager);
-muleContext.registry.registerObject(MuleProperties.OBJECT_SECURITY_MANAGER, new MuleSecurityManager());
-muleContext.registry.registerObject(MuleProperties.OBJECT_MULE_ENDPOINT_FACTORY, new DefaultEndpointFactory());
+muleContext.registry.registerObject(MuleProperties.OBJECT_QUEUE_MANAGER, queueManager)
+muleContext.registry.registerObject(MuleProperties.OBJECT_SECURITY_MANAGER, new MuleSecurityManager())
+muleContext.registry.registerObject(MuleProperties.OBJECT_MULE_ENDPOINT_FACTORY, new DefaultEndpointFactory())
 
-defaultThreadingProfile = new ChainedThreadingProfile();
-defaultThreadingProfile.setThreadWaitTimeout(30);
-defaultThreadingProfile.setMaxThreadsActive(10);
-defaultThreadingProfile.setMaxThreadsIdle(10);
-defaultThreadingProfile.setMaxBufferSize(0);
-defaultThreadingProfile.setThreadTTL(60000);
-defaultThreadingProfile.setPoolExhaustedAction(ThreadingProfile.WHEN_EXHAUSTED_RUN);
-muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_COMPONENT_THREADING_PROFILE,
-            new ChainedThreadingProfile(defaultThreadingProfile));
+defaultThreadingProfile = new ChainedThreadingProfile()
+defaultThreadingProfile.setThreadWaitTimeout(30)
+defaultThreadingProfile.setMaxThreadsActive(10)
+defaultThreadingProfile.setMaxThreadsIdle(10)
+defaultThreadingProfile.setMaxBufferSize(0)
+defaultThreadingProfile.setThreadTTL(60000)
+defaultThreadingProfile.setPoolExhaustedAction(ThreadingProfile.WHEN_EXHAUSTED_RUN)
+
+muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_SERVICE_THREADING_PROFILE,
+            new ChainedThreadingProfile(defaultThreadingProfile))
 muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_MESSAGE_RECEIVER_THREADING_PROFILE,
-            new ChainedThreadingProfile(defaultThreadingProfile));
+            new ChainedThreadingProfile(defaultThreadingProfile))
 muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_MESSAGE_DISPATCHER_THREADING_PROFILE,
-            new ChainedThreadingProfile(defaultThreadingProfile));
-muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_CONNECTION_STRATEGY, new SingleAttemptConnectionStrategy());
+            new ChainedThreadingProfile(defaultThreadingProfile))
+muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_RETRY_POLICY_TEMPLATE, new NoRetryPolicyTemplate())
 
 epFactory = muleContext.registry.lookupEndpointFactory()
 
