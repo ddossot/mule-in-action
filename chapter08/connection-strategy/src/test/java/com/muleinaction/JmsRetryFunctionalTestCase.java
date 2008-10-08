dcd52f81f1fbd7e397bbde394b790b66bbe9f63f
john.demic <john.demic@gmail.com>
2008-10-08 21:36:16 +0000

    added functional tests for Chapter 8's examples

---
+++ chapter08/connection-strategy/src/test/java/com/muleinaction/JmsRetryFunctionalTestCase.java
@@ -0,0 +1,24 @@
+package com.muleinaction;
+
+import org.mule.api.service.Service;
+import org.mule.tck.FunctionalTestCase;
+
+/**
+ * @author John D'Emic (john.demic@gmail.com)
+ */
+public class JmsRetryFunctionalTestCase extends FunctionalTestCase {
+
+	@Override
+	protected String getConfigResources() {
+		return "conf/jms-retry-config.xml";
+	}
+
+	public void testCorrectlyInitialized() throws Exception {
+		final Service service = muleContext.getRegistry().lookupService(
+				"jmsRetryService");
+
+		assertNotNull(service);
+		assertEquals("jmsRetryModel", service.getModel().getName());
+	}
+
+}
