86c4bc9e5365b5ad47b54017a028fb15369e6b67
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2008-10-14 06:03:19 +0000

    Added Mule event context sample

---
+++ chapter13/mule-event-context/src/main/java/com/clood/component/Md5FileHasher.java
@@ -0,0 +1,41 @@
+package com.clood.component;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.lifecycle.Callable;
+import org.mule.transport.file.FileConnector;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class Md5FileHasher implements Callable {
+
+    private String sourceFolder;
+
+    private String fileConnectorName;
+
+    public void setSourceFolder(final String sourceFolder) {
+        this.sourceFolder = sourceFolder;
+    }
+
+    public void setFileConnector(final FileConnector fileConnector) {
+        this.fileConnectorName = fileConnector.getName();
+    }
+
+    // <start id="EventContext-Md5FileHasher"/>
+    public Object onCall(final MuleEventContext eventContext) throws Exception {
+
+        final String fileName = eventContext.transformMessageToString();
+
+        final MuleMessage requestedFileMessage = eventContext.requestEvent(
+                "file://" + sourceFolder + "/" + fileName + "?connector="
+                        + fileConnectorName, 0);
+
+        eventContext.setStopFurtherProcessing(true);
+
+        return requestedFileMessage != null ? DigestUtils
+                .md5Hex(requestedFileMessage.getPayloadAsBytes()) : null;
+    }
+    // <end id="EventContext-Md5FileHasher"/>
+}
