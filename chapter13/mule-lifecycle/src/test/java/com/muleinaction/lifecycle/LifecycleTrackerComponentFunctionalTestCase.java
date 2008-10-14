649d84d933d10be92416a6b52264d9768aa5727f
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2008-10-14 05:23:54 +0000

    Added lifecycle tracking example

---
+++ chapter13/mule-lifecycle/src/test/java/com/muleinaction/lifecycle/LifecycleTrackerComponentFunctionalTestCase.java
@@ -0,0 +1,37 @@
+package com.muleinaction.lifecycle;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.mule.api.MuleContext;
+import org.mule.module.client.MuleClient;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class LifecycleTrackerComponentFunctionalTestCase {
+
+    @Test
+    public void trackLifecycle() throws Exception {
+        LifecycleTrackerComponent.getTracker().clear();
+
+        // exercise the target component but do not test anything here
+        final MuleClient muleClient = new MuleClient(
+                "conf/lifecycle-config.xml");
+        final MuleContext muleContext = muleClient.getMuleContext();
+        muleContext.start();
+
+        muleClient.sendDirect("DummyService", null, null, null);
+
+        muleContext.dispose();
+        muleClient.dispose();
+
+        final String tracking = LifecycleTrackerComponent.getTracker()
+                .toString();
+
+        assertEquals(
+                "[springSetProperty, setMuleContext, springInitialize, initialise, start, start, stop, stop, dispose, dispose, springDestroy]",
+                tracking);
+    }
+
+}
