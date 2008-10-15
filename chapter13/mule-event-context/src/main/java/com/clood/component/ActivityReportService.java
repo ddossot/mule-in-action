a03a4657b0741f7a3ae63df5d0cb346bdbb40b71
ddossot <david@dossot.net>
2008-10-15 00:34:06 +0000

    Added shell of ActivityReportService

---
+++ chapter13/mule-event-context/src/main/java/com/clood/component/ActivityReportService.java
@@ -0,0 +1,16 @@
+package com.clood.component;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class ActivityReportService implements Callable {
+
+    public Object onCall(final MuleEventContext eventContext) throws Exception {
+        // TODO validate client, return OK/ERROR and control dispatch
+        return null;
+    }
+
+}
