f4eec3bf19127d43ce2b01f9f8f189ef819d5a3d
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2008-10-15 17:06:50 +0000

    Begin to add samples for Chapter 10

---
+++ chapter10/exception-strategy/src/main/java/com/clood/billing/BillingServiceImpl.java
@@ -0,0 +1,10 @@
+package com.clood.billing;
+
+import com.clood.BusinessException;
+
+public class BillingServiceImpl {
+
+    public Object process(Object object) {
+        throw new BusinessException();
+    }
+}
