4d96fde78a42d86f0142eac3dc73ba7fed5a8436
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2008-10-15 00:34:06 +0000

    Added shell of ActivityReportService

---
+++ chapter13/mule-event-context/src/main/java/com/clood/model/Client.java
@@ -0,0 +1,40 @@
+package com.clood.model;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class Client {
+
+	private final long id;
+
+	private final String title;
+
+	private final String lastName;
+
+	public Client(final long id, final String title, final String lastName) {
+		this.id = id;
+		this.title = title;
+		this.lastName = lastName;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this,
+				ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+}
