17325884fc4a362f0e35e75771b4f9296abf9e96
ddossot <david@dossot.net>
2008-10-19 19:18:15 +0000

    Added demonstration of byte caching and original payload features

--- chapter13/mule-event-context/src/test/java/com/muleinaction/eventcontext/ProspectingMessage.java
+++ chapter13/mule-event-context/src/test/java/com/muleinaction/eventcontext/ProspectingMessage.java
@@ -4,13 +4,18 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigInteger;
+import java.util.Collections;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
 import org.mule.module.client.MuleClient;
+import org.mule.transformer.AbstractDiscoverableTransformer;
+import org.mule.transport.AbstractMessageAdapter;
 
 import edu.emory.mathcs.backport.java.util.Arrays;
 
@@ -19,11 +24,42 @@ import edu.emory.mathcs.backport.java.util.Arrays;
  */
 public class ProspectingMessage {
 
+    public final static class BigIntegerToBytesTransformer extends
+            AbstractDiscoverableTransformer {
+
+        public int usageCount = 0;
+
+        public BigIntegerToBytesTransformer() {
+            super();
+            registerSourceType(BigInteger.class);
+            setReturnClass(byte[].class);
+        }
+
+        @Override
+        protected Object doTransform(final Object src, final String encoding)
+                throws TransformerException {
+
+            usageCount++;
+
+            return ((BigInteger) src).toByteArray();
+        }
+
+    }
+
     private static MuleClient client;
+
+    private static BigIntegerToBytesTransformer BITBT = new BigIntegerToBytesTransformer();
 
     @BeforeClass
     public static void bootstrapMule() throws Exception {
         client = new MuleClient(true);
+
+        client.getMuleContext().getRegistry().registerTransformer(BITBT);
+    }
+
+    @Before
+    public void resetBigIntegerToBytesTransformerUsageCount() {
+        BITBT.usageCount = 0;
     }
 
     @AfterClass
@@ -33,31 +69,84 @@ public class ProspectingMessage {
 
     @Test
     public void defaultTransformersExist() {
+        // we should have more than our own transformer in the registry
         assertTrue(client.getMuleContext().getRegistry().getTransformers()
-                .size() > 0);
+                .size() > 1);
     }
 
     @Test
-    public void stringPayload() throws Exception {
+    public void stringPayloadAsBytesAndString() throws Exception {
         final String payload = "foo";
 
         final MuleMessage message = new DefaultMuleMessage(payload);
 
-        assertEquals(payload, message.getPayload());
-        assertEquals(payload, message.getPayloadAsString());
         assertTrue(Arrays.equals(payload.getBytes(message.getEncoding()),
                 message.getPayloadAsBytes()));
+
+        assertEquals(payload, message.getPayloadAsString());
+
+        // the bytes and string rendering do no alter the payload
+        assertEquals(payload, message.getPayload());
+        assertEquals(payload, message.getOrginalPayload());
     }
 
     @Test
-    public void serializablePayload() throws Exception {
+    public void serializablePayloadAsBytesAndString() throws Exception {
         final BigInteger payload = BigInteger.valueOf(123L);
 
         final MuleMessage message = new DefaultMuleMessage(payload);
 
+        assertEquals(0, BITBT.usageCount);
+
+        assertTrue(Arrays.equals(payload.toByteArray(), message
+                .getPayloadAsBytes()));
+
+        assertEquals(new String(payload.toByteArray(), message.getEncoding()),
+                message.getPayloadAsString());
+
+        assertEquals(1, BITBT.usageCount);
+
+        assertTrue(Arrays.equals(payload.toByteArray(), message
+                .getPayloadAsBytes()));
+
+        assertEquals(new String(payload.toByteArray(), message.getEncoding()),
+                message.getPayloadAsString());
+
+        // the usage count has not changed because we have hit the byte cache
+        // inside the message
+        assertEquals(1, BITBT.usageCount);
+
+        // the bytes and string rendering do no alter the payload
         assertEquals(payload, message.getPayload());
-        assertEquals(payload.toString(), message.getPayloadAsString());
-        assertTrue(Arrays.equals(payload.toString().getBytes(
-                message.getEncoding()), message.getPayloadAsBytes()));
+        assertEquals(payload, message.getOrginalPayload());
+    }
+
+    @Test
+    public void serializablePayloadTransformation() throws Exception {
+        final BigInteger payload = BigInteger.valueOf(987L);
+
+        final MuleMessage message = new DefaultMuleMessage(
+        // we do not use DefaultMessageAdapter because its payload is mutable
+                // which allows the original payload to change
+                new AbstractMessageAdapter() {
+                    private static final long serialVersionUID = 3953815192447005350L;
+
+                    public Object getPayload() {
+                        return payload;
+                    }
+                });
+
+        assertEquals(0, BITBT.usageCount);
+
+        // usually the transformers are defined on the endpoints
+        message.applyTransformers(Collections.singletonList(BITBT));
+        assertEquals(1, BITBT.usageCount);
+
+        assertTrue(message.getPayload() instanceof byte[]);
+
+        assertTrue(Arrays.equals(payload.toByteArray(), (byte[]) message
+                .getPayload()));
+
+        assertEquals(payload, message.getOrginalPayload());
     }
 }
