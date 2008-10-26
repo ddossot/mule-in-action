a0685a6725b07eb67582d4eb2f093ec5cc237587
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2008-10-26 06:47:02 +0000

    Reviewed cache to effectively cache payload only and not the whole MuleMessage

--- chapter13/component-interceptor/src/main/java/com/clood/interceptor/PayloadCacheInterceptor.java
+++ chapter13/component-interceptor/src/main/java/com/clood/interceptor/PayloadCacheInterceptor.java
@@ -1,7 +1,9 @@
 package com.clood.interceptor;
 
-import net.sf.ehcache.constructs.blocking.SelfPopulatingCache;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
 
+import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleException;
 import org.mule.api.MuleMessage;
 import org.mule.api.interceptor.Interceptor;
@@ -12,16 +14,29 @@ import org.mule.api.interceptor.Invocation;
  */
 public class PayloadCacheInterceptor implements Interceptor {
 
-    private SelfPopulatingCache cache;
+    private Ehcache cache;
 
-    public void setCache(final SelfPopulatingCache cache) {
+    public void setCache(final Ehcache cache) {
         this.cache = cache;
     }
 
     public MuleMessage intercept(final Invocation invocation)
             throws MuleException {
 
-        return (MuleMessage) cache.get(invocation).getValue();
+        final MuleMessage currentMessage = invocation.getMessage();
+        final Object key = currentMessage.getPayload();
+        final Element cachedElement = cache.get(key);
+
+        if (cachedElement != null) {
+            return new DefaultMuleMessage(cachedElement.getObjectValue(),
+                    currentMessage);
+        }
+
+        // we don't synchronize so several threads can compete to fill the cache
+        // for the same key: this is rare enough to be acceptable
+        final MuleMessage result = invocation.invoke();
+        cache.put(new Element(key, result.getPayload()));
+        return result;
     }
 
 }
