e8a520ec479c8fea946f192625a8d28b49325207
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2008-10-30 05:01:41 +0000

    Added code extraction tags

--- chapter07/fault-tolerance/src/main/java/com/muleinaction/service/CorrelationTimeOutListener.java
+++ chapter07/fault-tolerance/src/main/java/com/muleinaction/service/CorrelationTimeOutListener.java
@@ -14,7 +14,42 @@ import org.mule.module.client.MuleClient;
 /**
  * @author David Dossot (david@dossot.net)
  */
-public class CorrelationTimeOutListener implements RoutingNotificationListener {
+// <start id="CorrelationTimeOutListener"/>
+public final class CorrelationTimeOutListener implements
+        RoutingNotificationListener {
+
+    public void onNotification(final ServerNotification notification) {
+        if (notification.getAction() != RoutingNotification.CORRELATION_TIMEOUT) {
+            return;
+        }
+
+        final MuleMessageCollection messageCollection = (MuleMessageCollection) notification
+                .getSource();
+        // <end id="CorrelationTimeOutListener"/>
+
+        try {
+            // we assume here that we care only about the first message of the
+            // aggregation collection
+            final MuleMessage uncorrelatedMessage = messageCollection
+                    .getMessagesAsArray()[0];
+
+            final Map<String, Object> properties = new HashMap<String, Object>();
+
+            for (final Object propertyName : uncorrelatedMessage
+                    .getPropertyNames()) {
+                properties.put(propertyName.toString(), uncorrelatedMessage
+                        .getProperty(propertyName.toString()));
+            }
+
+            muleClient.sendNoReceive(dlqAddress, uncorrelatedMessage
+                    .getPayload(), properties);
+
+        } catch (final MuleException me) {
+            // here we should log a serialized form of the message, using a
+            // specific file appender that target a DLQ log file
+            System.err.println(messageCollection);
+        }
+    }
 
     private String dlqAddress;
 
@@ -26,38 +61,5 @@ public class CorrelationTimeOutListener implements RoutingNotificationListener {
 
     public void initialize() throws MuleException {
         muleClient = new MuleClient();
-    }
-
-    public void onNotification(final ServerNotification notification) {
-        if (notification.getAction() != RoutingNotification.CORRELATION_TIMEOUT) {
-            return;
-        }
-
-        final MuleMessageCollection messageCollection =
-                (MuleMessageCollection) notification.getSource();
-
-        try {
-            // we assume here that we care only about the first message of the
-            // aggregation collection
-            final MuleMessage uncorrelatedMessage =
-                    messageCollection.getMessagesAsArray()[0];
-
-            final Map<String, Object> properties =
-                    new HashMap<String, Object>();
-
-            for (final Object propertyName : uncorrelatedMessage.getPropertyNames()) {
-                properties.put(
-                        propertyName.toString(),
-                        uncorrelatedMessage.getProperty(propertyName.toString()));
-            }
-
-            muleClient.sendNoReceive(dlqAddress,
-                    uncorrelatedMessage.getPayload(), properties);
-
-        } catch (final MuleException me) {
-            // here we should log a serialized form of the message, using a
-            // specific file appender that target a DLQ log file
-            System.err.println(messageCollection);
-        }
     }
 }
