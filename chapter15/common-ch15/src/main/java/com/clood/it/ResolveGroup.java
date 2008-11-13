c2151af114a7913ed15e02fe262250edb14ff622
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2008-11-13 02:59:07 +0000

    flesh out jBPM example

---
+++ chapter15/common-ch15/src/main/java/com/clood/it/ResolveGroup.java
@@ -0,0 +1,34 @@
+package com.clood.it;
+
+import org.jbpm.graph.exe.ExecutionContext;
+import org.jbpm.graph.node.DecisionHandler;
+import org.jdom.xpath.XPath;
+import org.jdom.JDOMException;
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+
+import java.io.ByteArrayInputStream;
+
+public class ResolveGroup implements DecisionHandler {
+
+    private XPath groupExpression;
+
+    public ResolveGroup() {
+        try {
+            groupExpression = XPath.newInstance("//group");
+        } catch (JDOMException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String decide(ExecutionContext executionContext) throws Exception {
+
+        String account = (String) executionContext.getContextInstance().getVariable("incoming");
+
+        SAXBuilder builder = new SAXBuilder();
+        Document doc = builder.build(new ByteArrayInputStream(account.getBytes()));
+
+        return (groupExpression.valueOf(doc));
+    }
+
+}
