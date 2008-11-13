b28feeebbd2d8daeaede59c0534b4015e99d32f2
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2008-11-13 01:29:13 +0000

    Added examples for expression evaluators appendix

---
+++ appendices/expression-evaluators/src/main/java/com/muleinaction/component/ExpressionParser.java
@@ -0,0 +1,23 @@
+package com.muleinaction.component;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+import org.mule.util.expression.ExpressionEvaluatorManager;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class ExpressionParser implements Callable {
+
+    private String expression;
+
+    public void setExpression(final String expression) {
+        this.expression = expression;
+    }
+
+    public Object onCall(final MuleEventContext eventContext) throws Exception {
+        return ExpressionEvaluatorManager.parse(expression, eventContext
+                .getMessage(), true);
+    }
+
+}
