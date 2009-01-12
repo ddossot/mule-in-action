fac30ddafc253f8ed8b09b17a9a430be35d50e9a
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-01-12 05:20:29 +0000

    Examples for chapter 14

---
+++ chapter14/spring/script/messageEnricher.groovy
@@ -0,0 +1,13 @@
+import org.mule.api.MuleEventContext
+import org.mule.api.lifecycle.Callable
+
+class MessageEnricher implements Callable {
+
+    public Object onCall(MuleEventContext muleEventContext) {
+        def message = muleEventContext.getMessage()
+        if (message.payload =~ "STATUS: CRITICAL") {
+            message.setProperty("PRIORITY", 'HIGH')
+        }
+        return message
+    }
+}
---
+++ chapter14/spring/script/toUpper.groovy
@@ -0,0 +1,9 @@
+import org.mule.transformer.AbstractTransformer
+
+class ToUpperTransformer extends AbstractTransformer {
+
+    protected Object doTransform(Object o, String s) {
+        return o.toUpperCase();
+    }
+
+}
