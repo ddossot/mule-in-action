754a5c0a26b1a87f65a9821c1fde88dd050fe27a
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-01-16 17:22:03 +0000

    Functional tests for chapter 10 added

---
+++ chapter10/exception-strategy/src/test/java/com/muleinaction/ConnectionExceptionStrategyFunctionalTestCase.java
@@ -0,0 +1,24 @@
+package com.muleinaction;
+
+import org.mule.api.service.Service;
+import org.mule.tck.FunctionalTestCase;
+
+/**
+ * @author John D'Emic (john.demic@gmail.com)
+ */
+public class ConnectionExceptionStrategyFunctionalTestCase extends FunctionalTestCase {
+
+	@Override
+	protected String getConfigResources() {
+		return "conf/connector-exception-strategy-config.xml";
+	}
+
+	public void testCorrectlyInitialized() throws Exception {
+		final Service service = muleContext.getRegistry().lookupService(
+				"BillingService");
+
+		assertNotNull(service);
+		assertEquals("BillingModel", service.getModel().getName());
+	}
+
+}
