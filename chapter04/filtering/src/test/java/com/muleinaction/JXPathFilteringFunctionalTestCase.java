ecbf6cd801a64ce7f3692c75a113647066897369
john.demic <john.demic@gmail.com>
2009-01-19 22:39:21 +0000

    improved functional tests for header examples

--- chapter04/filtering/src/test/java/com/muleinaction/JXPathFilteringFunctionalTestCase.java
+++ chapter04/filtering/src/test/java/com/muleinaction/JXPathFilteringFunctionalTestCase.java
@@ -1,24 +1,48 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
+
+import com.muleinaction.common.Order;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class JXPathFilteringFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/jxpath-filtering-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/jxpath-filtering-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"jxpathFilteringService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "jxpathFilteringService");
 
-		assertNotNull(service);
-		assertEquals("jxpathFilteringModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("jxpathFilteringModel", service.getModel().getName());
+    }
+
+    public void testMessageConsumed() throws Exception {
+        Order order = new Order();
+        order.setStatus("FULFILLED");
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://messages.in", order, null);
+        MuleMessage response = muleClient.request("jms://messages.out", 2000);
+        assertNotNull(response);
+    }
+
+    public void testMessageNotConsumed() throws Exception {
+        Order order = new Order();
+        order.setStatus("CANCELLED");
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://messages.in", order, null);
+        MuleMessage response = muleClient.request("jms://messages.out", 2000);
+        assertNull(response);
+        response = muleClient.request("jms://errors", 2000);
+        assertNotNull(response);
+    }
 
 }
