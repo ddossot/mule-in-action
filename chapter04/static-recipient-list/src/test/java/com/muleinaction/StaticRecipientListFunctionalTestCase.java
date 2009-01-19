ef2a879a746ed8d4e6168d34443c1029c6af697a
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-01-19 23:07:26 +0000

    functional tests for static-recipient-list router improved.

--- chapter04/static-recipient-list/src/test/java/com/muleinaction/StaticRecipientListFunctionalTestCase.java
+++ chapter04/static-recipient-list/src/test/java/com/muleinaction/StaticRecipientListFunctionalTestCase.java
@@ -1,24 +1,37 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class StaticRecipientListFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/static-recipient-list-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/static-recipient-list-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"staticRecipientListService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "staticRecipientListService");
 
-		assertNotNull(service);
-		assertEquals("staticRecipientListModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("staticRecipientListModel", service.getModel().getName());
+    }
+
+    public void testMessageConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("vm://quoteData", "JAVA: 100", null);
+        MuleMessage response = muleClient.request("jms://brokerage.house.1", 2000);
+        assertNotNull(response);
+        response = muleClient.request("jms://brokerage.house.2", 2000);
+        assertNotNull(response);
+        response = muleClient.request("jms://brokerage.house.3", 2000);
+        assertNotNull(response); 
+    }
 
 }
