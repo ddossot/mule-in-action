43a2ccc47e0e7c4eaca33cd6adc6f32e50f3e5df
john.demic <john.demic@gmail.com>
2009-01-19 19:37:09 +0000

    functional tests for the selective-consumer examples have been improved.

--- chapter04/selective-consumer/src/test/java/com/muleinaction/SelectiveHeaderConsumerFunctionalTestCase.java
+++ chapter04/selective-consumer/src/test/java/com/muleinaction/SelectiveHeaderConsumerFunctionalTestCase.java
@@ -1,24 +1,47 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
+
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class SelectiveHeaderConsumerFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/selective-header-consumer-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/selective-header-consumer-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"selectiveHeaderConsumerService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "selectiveHeaderConsumerService");
 
-		assertNotNull(service);
-		assertEquals("selectiveHeaderConsumerModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("selectiveHeaderConsumerModel", service.getModel().getName());
+    }
+
+    public void testMessageConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        Map<String,String> properties = new HashMap<String,String>();
+        properties.put("STATUS", "OK");
+        muleClient.sendAsync("jms://messages.in", "PAYLOAD", properties);
+        MuleMessage response = muleClient.request("jms://messages.out", 2000);
+        assertNotNull(response);
+    }
+
+    public void testMessageNotConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://messages.in", "STATUS: CRITICAL", null);
+        MuleMessage response = muleClient.request("jms://messages.out", 2000);
+        assertNull(response);
+        response = muleClient.request("jms://messages.errors", 2000);
+        assertNotNull(response);
+    }
 
 }
