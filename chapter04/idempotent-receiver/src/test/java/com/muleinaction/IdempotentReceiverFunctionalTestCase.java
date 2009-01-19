addd751f2639255f10ed3ae46d634deb748fa36d
john.demic <john.demic@gmail.com>
2009-01-19 22:05:15 +0000

    improved functional tests for the idempotent-receiver

--- chapter04/idempotent-receiver/src/test/java/com/muleinaction/IdempotentReceiverFunctionalTestCase.java
+++ chapter04/idempotent-receiver/src/test/java/com/muleinaction/IdempotentReceiverFunctionalTestCase.java
@@ -1,24 +1,62 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import com.muleinaction.common.Order;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class IdempotentReceiverFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/idempotent-receiver-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/idempotent-receiver-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"idempotentReceiverService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "idempotentReceiverService");
 
-		assertNotNull(service);
-		assertEquals("idempotentReceiverModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("idempotentReceiverModel", service.getModel().getName());
+    }
+
+    public void testMessageConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        Map<String, String> properties = new HashMap<String, String>();
+
+        properties.put("orderId", "1234");
+        muleClient.sendAsync("jms://orders", getOrder(), properties);
+        MuleMessage response = muleClient.request("jms://duplicate.orders", 2000);
+        assertNull(response);
+    }
+
+    public void testMessageConsumedOnce() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        Map<String, String> properties = new HashMap<String, String>();
+
+        properties.put("orderId", "1234");
+        muleClient.sendAsync("jms://orders", getOrder(), properties);
+        muleClient.sendAsync("jms://orders", getOrder(), properties);
+
+        MuleMessage response = muleClient.request("jms://duplicate.orders", 2000);
+        assertNotNull(response);
+    }
+
+    Order getOrder() {
+        Order order = new Order();
+        order.setProductId(1234);
+        order.setPurchaserId(1234);
+        order.setStatus("PENDING");
+        return order;
+    }
+
 
 }
