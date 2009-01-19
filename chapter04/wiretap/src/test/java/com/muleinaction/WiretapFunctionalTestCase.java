785de31722efd4f9b840e548564c8afaef044edd
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-01-19 22:47:43 +0000

    functional tests for wiretap examples improved

--- chapter04/wiretap/src/test/java/com/muleinaction/WiretapFunctionalTestCase.java
+++ chapter04/wiretap/src/test/java/com/muleinaction/WiretapFunctionalTestCase.java
@@ -1,24 +1,32 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class WiretapFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/wiretap-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/wiretap-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"wiretapService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "wiretapService");
 
-		assertNotNull(service);
-		assertEquals("wiretapModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("wiretapModel", service.getModel().getName());
+    }
+
+    public void testMessageConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://orders", "Order Data", null);
+     
+    }
 
 }
