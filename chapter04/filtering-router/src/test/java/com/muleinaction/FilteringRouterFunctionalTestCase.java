3fec5cc9dcbfd50e723543ea7d86aa1e17e04706
john.demic <john.demic@gmail.com>
2009-01-20 02:34:37 +0000

    build working now, need to fix mvn issues with the functional tests for the idempotent-receiver and chaining-router

--- chapter04/filtering-router/src/test/java/com/muleinaction/FilteringRouterFunctionalTestCase.java
+++ chapter04/filtering-router/src/test/java/com/muleinaction/FilteringRouterFunctionalTestCase.java
@@ -1,24 +1,45 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class FilteringRouterFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/filtering-router-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/filtering-router-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"filteringRouterService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "filteringRouterService");
 
-		assertNotNull(service);
-		assertEquals("filteringRouterModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("filteringRouterModel", service.getModel().getName());
+    }
+
+    public void testMessageConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://fares", "JFK-SJC: 1000.00", null);
+        muleClient.sendAsync("jms://fares", "JFK-SJC: 2000.00", null);
+
+        MuleMessage response = muleClient.request("jms://cheapestFares", 2000);
+        assertNotNull(response);
+    }
+
+    /*
+    public void testMessageNotConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://messages.in", "STATUS: CRITICAL", null);
+        MuleMessage response = muleClient.request("jms://messages.out", 2000);
+        assertNull(response);
+        response = muleClient.request("jms://messages.errors", 2000);
+        assertNotNull(response);
+    } */
 
 }
