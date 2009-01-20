86e3e160c0e19af0620244ba003a0e98c5ff012f
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-01-20 01:14:33 +0000

    improved functional tests for the async-request-reply examples.

--- chapter04/async-request-reply/src/test/java/com/muleinaction/AsyncRequestReplyFunctionalTestCase.java
+++ chapter04/async-request-reply/src/test/java/com/muleinaction/AsyncRequestReplyFunctionalTestCase.java
@@ -1,24 +1,34 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class AsyncRequestReplyFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/async-request-reply-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/async-request-reply-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"asyncRequestReplyService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "asyncRequestReplyService");
 
-		assertNotNull(service);
-		assertEquals("asyncRequestReplyModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("asyncRequestReplyModel", service.getModel().getName());
+    }
+
+    public void testMessageConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+       
+        MuleMessage response = muleClient.send("vm://flightRequests","JFK",null);
+        assertNotNull(response);
+    }
+
 
 }
