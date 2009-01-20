0e5d35ac471d6f5713ce1f078decefd12743da7d
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-01-20 15:57:09 +0000

    improved functional test cases for transaction examples

---
+++ chapter10/single-resource-transaction/src/test/java/com/muleinaction/NonJoinedTransactionFunctionalTestCase.java
@@ -0,0 +1,39 @@
+package com.muleinaction;
+
+import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
+
+/**
+ * @author John D'Emic (john.demic@gmail.com)
+ */
+public class NonJoinedTransactionFunctionalTestCase extends FunctionalTestCase {
+
+    @Override
+    protected String getConfigResources() {
+        return "conf/jms-nonjoined-transaction-config.xml";
+    }
+
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "staticRecipientListService");
+
+        assertNotNull(service);
+        assertEquals("staticRecipientListModel", service.getModel().getName());
+    }
+
+    public void testMessageConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://application-response-times", "DATA-1", null);
+
+        MuleMessage response = muleClient.request("jms://operational-database", 2000);
+        assertNotNull(response);
+        assertEquals("DATA-1", response.getPayloadAsString());
+        response = muleClient.request("jms://data-warehouse", 2000);
+        assertNotNull(response);
+        assertEquals("DATA-1", response.getPayloadAsString());
+
+    }
+
+}
