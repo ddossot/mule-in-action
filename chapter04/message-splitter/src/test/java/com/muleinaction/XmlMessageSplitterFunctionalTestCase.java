a7e8c0be5ecfcadaf566674e5a1e33509e6652c3
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-01-21 06:47:32 +0000

    improved functional tests for xml-message-splitter example

--- chapter04/message-splitter/src/test/java/com/muleinaction/XmlMessageSplitterFunctionalTestCase.java
+++ chapter04/message-splitter/src/test/java/com/muleinaction/XmlMessageSplitterFunctionalTestCase.java
@@ -1,24 +1,69 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class XmlMessageSplitterFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/xml-message-splitter-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/xml-message-splitter-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"xmlMessageSplitterService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "xmlMessageSplitterService");
 
-		assertNotNull(service);
-		assertEquals("xmlMessageSplitterModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("xmlMessageSplitterModel", service.getModel().getName());
+    }
+
+
+    public void testMessageConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("vm://orders", getOrders(), null);
+
+        MuleMessage response = muleClient.request("vm://orders.fulfilled", 2000);
+        assertNotNull(response);
+
+        response = muleClient.request("vm://orders.fulfilled", 2000);
+        assertNotNull(response);
+
+        response = muleClient.request("vm://orders.fulfilled", 2000);
+        assertNull(response);
+
+        response = muleClient.request("vm://orders.pending", 2000);
+        assertNotNull(response);
+        response = muleClient.request("vm://orders.pending", 2000);
+        assertNull(response);
+
+        response = muleClient.request("vm://orders.unknown", 2000);
+        assertNotNull(response);
+        response = muleClient.request("vm://orders.unknown", 2000);
+        assertNull(response);
+    }
+
+    String getOrders() {
+        StringBuilder result = new StringBuilder("<orders>");
+        result.append(getOrder("1", "1", "FULFILLED"));
+        result.append(getOrder("1", "1", "FULFILLED"));
+        result.append(getOrder("1", "1", "PENDING"));
+        result.append(getOrder("1", "1", "CANCELLED"));
+        result.append("</orders>");
+        return result.toString();
+    }
+
+    String getOrder(String purchaserId, String productId, String status) {
+        return String.format("<order>" +
+                "<purchaserId>%s</purchaserId>" +
+                "<productId>%s</productId>" +
+                "<status>%s</status> " +
+                "</order>", purchaserId, productId, status);
+    }
 
 }
