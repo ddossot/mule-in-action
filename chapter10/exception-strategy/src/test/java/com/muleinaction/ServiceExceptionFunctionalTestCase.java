d8dc63c01e195d36646d50214d8545e4b5d76ade
john.demic <john.demic@gmail.com>
2009-01-21 05:46:50 +0000

    functional tests for exception strategies improved

--- chapter10/exception-strategy/src/test/java/com/muleinaction/ServiceExceptionFunctionalTestCase.java
+++ chapter10/exception-strategy/src/test/java/com/muleinaction/ServiceExceptionFunctionalTestCase.java
@@ -1,24 +1,35 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class ServiceExceptionFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/service-exception-strategy-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/service-exception-strategy-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"forwardingConsumerService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "forwardingConsumerService");
 
-		assertNotNull(service);
-		assertEquals("forwardingConsumerModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("forwardingConsumerModel", service.getModel().getName());
+    }
+
+    public void testMessageConsumed() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://messages.in", "STATUS: CANCELLED", null);
+        MuleMessage response = muleClient.request("jms://messages.out", 2000);
+        assertNull(response);
+        
+    }
+
 
 }
