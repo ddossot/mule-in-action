a6e2e79a7e54c7ff786aebe6a94407407025bc74
john.demic <john.demic@gmail.com>
2009-01-21 16:03:18 +0000

    add chapter 12 examples to the build

--- chapter12/message-enricher/src/test/java/com/clood/MessageEnricherFunctionalTest.java
+++ chapter12/message-enricher/src/test/java/com/clood/MessageEnricherFunctionalTest.java
@@ -1,10 +1,25 @@
 package com.clood;
 
 import org.mule.tck.FunctionalTestCase;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.RequestContext;
 
-public class MessageEnricherFunctionalTest  extends FunctionalTestCase {
+public class MessageEnricherFunctionalTest extends FunctionalTestCase {
 
     protected String getConfigResources() {
         return "conf/message-enricher-conf.xml";
+    }
+
+    public void testMessageTransformation() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://in", "TEST_PAYLOAD", null);
+
+        MuleMessage response = muleClient.request("jms://out", 2000);
+
+        assertEquals("***[MESSAGE=TEST_PAYLOAD]***", response.getPayload());
+        assertNotNull(response.getProperty("ORGANIZATION", true));
+        assertEquals("CLOOD", response.getProperty("ORGANIZATION", true));
+
     }
 }
