e6a54ce9219b45f4017c7ac5aeb4c2f9fbb5c089
ddossot <david@dossot.net>
2009-01-29 04:55:25 +0000

    Reduced dependency set

--- chapter04/filtering-router/src/main/java/com/muleinaction/FareSelectionService.java
+++ chapter04/filtering-router/src/main/java/com/muleinaction/FareSelectionService.java
@@ -1,26 +1,27 @@
 package com.muleinaction;
 
 import java.util.List;
+
 import javax.jms.ObjectMessage;
 
-import com.muleinaction.common.*;
+import com.muleinaction.common.Fare;
 
 public class FareSelectionService {
 
-    public Fare selectFares(Object object) throws Exception {
+    public Fare selectFares(final Object object) throws Exception {
 
-        List fares = (List) object;
+        final List fares = (List) object;
 
         Fare cheapestFare = null;
 
-        for (int i=0; i < fares.size(); i++) {
-            ObjectMessage message = (ObjectMessage) fares.get(i);
-            Fare currentFare = (Fare) message.getObject();
-               
+        for (int i = 0; i < fares.size(); i++) {
+            final ObjectMessage message = (ObjectMessage) fares.get(i);
+            final Fare currentFare = (Fare) message.getObject();
+
             if (cheapestFare == null)
-                cheapestFare = currentFare; 
+                cheapestFare = currentFare;
             else if (currentFare.getPrice() < cheapestFare.getPrice())
-                cheapestFare = currentFare; 
+                cheapestFare = currentFare;
         }
 
         return cheapestFare;
--- chapter04/filtering-router/src/test/java/com/muleinaction/FilteringRouterFunctionalTestCase.java
+++ chapter04/filtering-router/src/test/java/com/muleinaction/FilteringRouterFunctionalTestCase.java
@@ -1,9 +1,11 @@
 package com.muleinaction;
 
-import org.mule.api.service.Service;
 import org.mule.api.MuleMessage;
-import org.mule.tck.FunctionalTestCase;
+import org.mule.api.service.Service;
 import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+import com.muleinaction.common.Fare;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
@@ -24,22 +26,26 @@ public class FilteringRouterFunctionalTestCase extends FunctionalTestCase {
     }
 
     public void testMessageConsumed() throws Exception {
-        MuleClient muleClient = new MuleClient(muleContext);
-        muleClient.sendAsync("jms://fares", "JFK-SJC: 1000.00", null);
-        muleClient.sendAsync("jms://fares", "JFK-SJC: 2000.00", null);
+        final MuleClient muleClient = new MuleClient(muleContext);
+        muleClient.sendAsync("jms://fares", new Fare("UA", "JFK-SJC", 1000.00),
+                null);
+        muleClient.sendAsync("jms://fares", new Fare("UA", "JFK-SJC", 2000.00),
+                null);
 
-        MuleMessage response = muleClient.request("jms://cheapestFares", 2000);
+        final MuleMessage response = muleClient.request("jms://cheapestFares",
+                2000);
+
         assertNotNull(response);
     }
 
     /*
-    public void testMessageNotConsumed() throws Exception {
-        MuleClient muleClient = new MuleClient(muleContext);
-        muleClient.sendAsync("jms://messages.in", "STATUS: CRITICAL", null);
-        MuleMessage response = muleClient.request("jms://messages.out", 2000);
-        assertNull(response);
-        response = muleClient.request("jms://messages.errors", 2000);
-        assertNotNull(response);
-    } */
+     * public void testMessageNotConsumed() throws Exception { MuleClient
+     * muleClient = new MuleClient(muleContext);
+     * muleClient.sendAsync("jms://messages.in", "STATUS: CRITICAL", null);
+     * MuleMessage response = muleClient.request("jms://messages.out", 2000);
+     * assertNull(response); response =
+     * muleClient.request("jms://messages.errors", 2000);
+     * assertNotNull(response); }
+     */
 
 }
