7c7eaaf350f7248142271bce70c9ef516c9b7e8d
ddossot <david@dossot.net>
2009-02-01 22:46:05 +0000

    Added code extraction tags

--- appendices/expression-evaluators/src/main/java/com/muleinaction/component/ExpressionParser.java
+++ appendices/expression-evaluators/src/main/java/com/muleinaction/component/ExpressionParser.java
@@ -15,9 +15,11 @@ public class ExpressionParser implements Callable {
         this.expression = expression;
     }
 
+		// <start id="ExprParser-Code" />
     public Object onCall(final MuleEventContext eventContext) throws Exception {
         return ExpressionEvaluatorManager.parse(expression, eventContext
                 .getMessage(), true);
     }
+		// <end id="ExprParser-Code" />
 
 }
