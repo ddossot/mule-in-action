e747ad8910696fa983b2b62b85558eea1b6fbe34
john.demic <john.demic@gmail.com>
2009-02-07 17:42:03 +0000

    continued refacotring to make examples Clood-Compliant

---
+++ chapter04/common-ch04/src/main/java/com/muleinaction/common/FarmStatus.java
@@ -0,0 +1,33 @@
+package com.muleinaction.common;
+
+import java.io.Serializable;
+
+public class FarmStatus implements Serializable {
+
+    String name;
+    long vmCount;
+
+    public FarmStatus() {
+    }
+
+    public FarmStatus(String name, long vmCount) {
+        this.name = name;
+        this.vmCount = vmCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getVmCount() {
+        return vmCount;
+    }
+
+    public void setVmCount(long vmCount) {
+        this.vmCount = vmCount;
+    }
+}
