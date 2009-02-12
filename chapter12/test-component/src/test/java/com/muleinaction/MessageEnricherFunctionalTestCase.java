cb67adb311ed64408098c3ace09aecac5390a42c
john.demic <john.demic@gmail.com>
2009-02-12 21:13:38 +0000

    begin to add examples for the test component

---
+++ chapter12/test-component/src/test/java/com/muleinaction/MessageEnricherFunctionalTestCase.java
@@ -0,0 +1,21 @@
+package com.muleinaction;
+
+import org.mule.tck.FunctionalTestCase;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+
+public class MessageEnricherFunctionalTestCase extends FunctionalTestCase {
+    protected String getConfigResources() {
+        return "conf/test-component-delay.xml";
+    }
+
+    public void testMessageTimedOut() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+
+        muleClient.sendAsync("jms://in", "TEST_PAYLOAD", null);
+
+        MuleMessage response = muleClient.request("jms://out", 2000);
+        assertNull(response);
+
+    }
+}
