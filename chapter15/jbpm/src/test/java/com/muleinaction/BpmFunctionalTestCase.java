e32b8949bc86d03f92e35f8fa1fb598230f92400
john.demic <john.demic@gmail.com>
2009-02-13 22:36:28 +0000

    continue improvements on jBPM example

--- chapter15/jbpm/src/test/java/com/muleinaction/BpmFunctionalTestCase.java
+++ chapter15/jbpm/src/test/java/com/muleinaction/BpmFunctionalTestCase.java
@@ -2,19 +2,32 @@ package com.muleinaction;
 
 import org.mule.tck.FunctionalTestCase;
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
 
 public class BpmFunctionalTestCase extends FunctionalTestCase {
+
+    private static String PAYLOAD_OPS_XML = "<account>\n" +
+            "    <name>John</name>\n" +
+            "    <group>Operations</group>\n" +
+            "</account>";
 
     protected String getConfigResources() {
         return "conf/provisioning-config.xml";
     }
 
     public void testCorrectlyInitialized() throws Exception {
-           final Service service = muleContext.getRegistry().lookupService(
-                   "bpmSink");
+        final Service service = muleContext.getRegistry().lookupService(
+                "createAccount");
 
-           assertNotNull(service);
-           assertEquals("accountProvisioningModel", service.getModel().getName());
-       }
+        assertNotNull(service);
+        assertEquals("accountProvisioningModel", service.getModel().getName());
+
+        MuleClient muleClient = new MuleClient(muleContext);                   
+        //muleClient.sendAsync("jms://it.provisioning.ldap","Test",null);
+        muleClient.send("jms://it.provisioning.requests", PAYLOAD_OPS_XML, null);
+        MuleMessage response = muleClient.request("jms://messages.out", 15000);
+
+    }
 
 }
