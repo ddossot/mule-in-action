da6374cb0c38ca0d28290d97fa6ce4dfdd54e3b8
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-02-13 15:40:09 +0000

    begin to add test component examples

---
+++ chapter12/test-component/src/test/java/com/muleinaction/ErrorQueueFunctionalTestCase.java
@@ -0,0 +1,22 @@
+package com.muleinaction;
+
+import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
+import org.mule.api.MuleMessage;
+
+public class ErrorQueueFunctionalTestCase extends FunctionalTestCase {
+
+    protected String getConfigResources() {
+        return "conf/test-component-exception.xml";
+    }
+
+    public void testExceptionRouted() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+
+        muleClient.sendAsync("jms://in", "TEST_PAYLOAD", null);
+
+        MuleMessage response = muleClient.request("jms://errors", 2000);
+        assertNotNull(response);
+
+    }
+}
