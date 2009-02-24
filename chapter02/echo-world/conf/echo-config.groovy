08010e3f46b2549a291fa3ed5da760d5e9ecb6fa
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2009-02-24 06:14:27 +0000

    Migrated to Mule 2.2.0

--- chapter02/echo-world/conf/echo-config.groovy
+++ chapter02/echo-world/conf/echo-config.groovy
@@ -34,6 +34,8 @@ muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_MESSAGE_RECEIV
             new ChainedThreadingProfile(defaultThreadingProfile))
 muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_MESSAGE_DISPATCHER_THREADING_PROFILE,
             new ChainedThreadingProfile(defaultThreadingProfile))
+muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_MESSAGE_REQUESTER_THREADING_PROFILE,
+            new ChainedThreadingProfile(defaultThreadingProfile))
 muleContext.registry.registerObject(MuleProperties.OBJECT_DEFAULT_RETRY_POLICY_TEMPLATE, new NoRetryPolicyTemplate())
 
 epFactory = muleContext.registry.lookupEndpointFactory()
