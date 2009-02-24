ac3b2de095820c9c0de1dbc84f1b475cfbc43ca6
ddossot <david@dossot.net>
2009-02-24 06:14:27 +0000

    Migrated to Mule 2.2.0

--- chapter02/echo-world/src/test/java/com/muleinaction/EchoWorldGroovyFunctionalTestCase.java
+++ chapter02/echo-world/src/test/java/com/muleinaction/EchoWorldGroovyFunctionalTestCase.java
@@ -7,16 +7,16 @@ import org.mule.module.scripting.builders.ScriptConfigurationBuilder;
  * @author David Dossot (david@dossot.net)
  */
 public class EchoWorldGroovyFunctionalTestCase extends
-		EchoWorldXmlFunctionalTestCase {
+        EchoWorldXmlFunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/echo-config.groovy";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/echo-config.groovy";
+    }
 
-	@Override
-	protected ConfigurationBuilder getBuilder() throws Exception {
-		return new ScriptConfigurationBuilder("groovy", getConfigurationResources());
-	}
+    @Override
+    protected ConfigurationBuilder getBuilder() throws Exception {
+        return new ScriptConfigurationBuilder("groovy", getConfigResources());
+    }
 
 }
