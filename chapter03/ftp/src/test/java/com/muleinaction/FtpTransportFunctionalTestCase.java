2fe3f51176d9642d524e7cc717ddf8f754a87190
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-02-25 05:06:49 +0000

    FTP examples upgraded to Mule 2.2.  Functional test cases improved

--- chapter03/ftp/src/test/java/com/muleinaction/FtpTransportFunctionalTestCase.java
+++ chapter03/ftp/src/test/java/com/muleinaction/FtpTransportFunctionalTestCase.java
@@ -2,23 +2,67 @@ package com.muleinaction;
 
 import org.mule.api.service.Service;
 import org.mule.tck.FunctionalTestCase;
+import org.mockftpserver.fake.FakeFtpServer;
+import org.mockftpserver.fake.UserAccount;
+import org.mockftpserver.fake.filesystem.FileSystem;
+import org.mockftpserver.fake.filesystem.FileEntry;
+import org.mockftpserver.fake.filesystem.UnixFakeFileSystem;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
+
+import java.io.File;
+
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class FtpTransportFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/ftp-inbound-config.xml";
-	}
+    FakeFtpServer fakeFtpServer;
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"ftpService");
+    private static String DEST_DIRECTORY = "./data/out";
 
-		assertNotNull(service);
-		assertEquals("ftpModel", service.getModel().getName());
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/ftp-inbound-config.xml";
+    }
+
+    protected void doSetUp() throws Exception {
+        super.doSetUp();
+        FileUtils.cleanDirectory(new File(DEST_DIRECTORY));
+    }
+
+    protected void suitePreSetUp() throws Exception {
+        startServer();
+    }
+
+    protected void suitePostTearDown() throws Exception {
+        stopServer();
+    }
+
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "ftpService");
+        assertNotNull(service);
+        assertEquals("ftpModel", service.getModel().getName());
+        // ToDo replace with a latch
+        Thread.sleep(5000);
+        assertEquals(1, FileUtils.listFiles(new File(DEST_DIRECTORY), new WildcardFileFilter("*.*"), null).size());
+    }
+
+    void startServer() {
+        fakeFtpServer = new FakeFtpServer();
+        fakeFtpServer.setServerControlPort(9879);
+        fakeFtpServer.addUserAccount(new UserAccount("ftp", "ftp", "/"));
+
+        FileSystem fileSystem = new UnixFakeFileSystem();
+        fileSystem.add(new FileEntry("/files/file1.txt", "MULEINACTION"));
+        fakeFtpServer.setFileSystem(fileSystem);
+        fakeFtpServer.start();
+    }
+
+    void stopServer() {
+        fakeFtpServer.stop();
+    }
 
 }
