a7df413e2b4e39a8d2bb2c2f5243ca6835beda71
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-02-25 04:28:53 +0000

    upgrade file transport examples to Mule 2.2.  Improved functional test cases additionally added.

--- chapter03/file/src/test/java/com/muleinaction/FileTransportFunctionalTestCase.java
+++ chapter03/file/src/test/java/com/muleinaction/FileTransportFunctionalTestCase.java
@@ -2,23 +2,49 @@ package com.muleinaction;
 
 import org.mule.api.service.Service;
 import org.mule.tck.FunctionalTestCase;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.UUID;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class FileTransportFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/file-config.xml";
-	}
+    private static String SOURCE_DIRECTORY = "./data/snapshot";
+       private static String DEST_DIRECTORY = "./data/archive";
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"fileService");
+       protected void doSetUp() throws Exception {
+           super.doSetUp();
+           FileUtils.cleanDirectory(new File(SOURCE_DIRECTORY));
+           FileUtils.cleanDirectory(new File(DEST_DIRECTORY));
+       }
 
-		assertNotNull(service);
-		assertEquals("fileModel", service.getModel().getName());
-	}
+       @Override
+       protected String getConfigResources() {
+           return "conf/file-xml-filter-config.xml";
+       }
+
+       public void testCorrectlyInitialized() throws Exception {
+           final Service service = muleContext.getRegistry().lookupService(
+                   "fileService");
+           assertNotNull(service);
+           assertEquals("fileModel", service.getModel().getName());
+       }
+
+       public void testFileIsArchived() throws IOException, InterruptedException {
+           String filename = "SNAPSHOT-" + UUID.randomUUID().toString() + ".xml";
+           BufferedWriter out = new BufferedWriter(new FileWriter(SOURCE_DIRECTORY + "/" + filename));
+           out.write("data");
+           out.close();
+           Thread.sleep(2000);
+           assertEquals(1,FileUtils.listFiles(new File(DEST_DIRECTORY),new WildcardFileFilter("*.*"), null).size());
+       }
+
 
 }
