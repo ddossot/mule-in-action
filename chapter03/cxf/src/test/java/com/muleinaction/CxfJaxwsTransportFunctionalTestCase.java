730d68855a7089077071b06aaa5285060eee38ca
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-02-25 04:20:13 +0000

    CXF examples upgraded to Mule 2.2

--- chapter03/cxf/src/test/java/com/muleinaction/CxfJaxwsTransportFunctionalTestCase.java
+++ chapter03/cxf/src/test/java/com/muleinaction/CxfJaxwsTransportFunctionalTestCase.java
@@ -1,24 +1,47 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class CxfJaxwsTransportFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/cxf-jaxws-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/cxf-jaxws-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"cxfJaxwsService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "cxfJaxwsService");
+        assertNotNull(service);
+        assertEquals("cxfJaxwsModel", service.getModel().getName());
+    }
 
-		assertNotNull(service);
-		assertEquals("cxfJaxwsModel", service.getModel().getName());
-	}
+    public void testMessageSentAndReceived() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        MuleMessage response = muleClient.send("http://localhost:9781/greeting",
+                SOAP_REQUEST, null);
+        assertNotNull(response);
+        assertEquals(SOAP_RESPONSE, response.getPayloadAsString());
+    }
+
+    private static String SOAP_REQUEST = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
+            "xmlns:jax=\"http://jaxws.cxf.muleinaction.com/\">\n" +
+            "   <soapenv:Header/>\n" +
+            "   <soapenv:Body>\n" +
+            "      <jax:sayGreeting>\n" +
+            "         <arg0>John</arg0>\n" +
+            "      </jax:sayGreeting>\n" +
+            "   </soapenv:Body>\n" +
+            "</soapenv:Envelope>";
+    
+    private static String SOAP_RESPONSE = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+            "<soap:Body><ns2:sayGreetingResponse xmlns:ns2=\"http://jaxws.cxf.muleinaction.com/\">" +
+            "<return>Hello, John</return></ns2:sayGreetingResponse></soap:Body></soap:Envelope>";
 
 }
--- chapter03/cxf/src/test/java/com/muleinaction/CxfSimpleTransportFunctionalTestCase.java
+++ chapter03/cxf/src/test/java/com/muleinaction/CxfSimpleTransportFunctionalTestCase.java
@@ -1,24 +1,48 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class CxfSimpleTransportFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/cxf-simple-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/cxf-simple-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"cxfSimpleService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "cxfSimpleService");
+        assertNotNull(service);
+        assertEquals("cxfSimpleModel", service.getModel().getName());
+    }
 
-		assertNotNull(service);
-		assertEquals("cxfSimpleModel", service.getModel().getName());
-	}
+    public void testMessageSentAndReceived() throws Exception {
+        MuleClient muleClient = new MuleClient(muleContext);
+        MuleMessage response = muleClient.send("http://localhost:9781/greeting",
+                SOAP_REQUEST, null);
+        assertNotNull(response);
+        assertEquals(SOAP_RESPONSE, response.getPayloadAsString());
+    }
+
+    private static String SOAP_REQUEST = "<soapenv:Envelope " +
+            "xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
+            "xmlns:sim=\"http://simple.cxf.muleinaction.com/\">\n" +
+            "   <soapenv:Header/>\n" +
+            "   <soapenv:Body>\n" +
+            "      <sim:getGreeting>\n" +
+            "         <sim:name>John</sim:name>\n" +
+            "      </sim:getGreeting>\n" +
+            "   </soapenv:Body>\n" +
+            "</soapenv:Envelope>";
+
+    private static String SOAP_RESPONSE = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+            "<soap:Body><ns1:getGreetingResponse xmlns:ns1=\"http://simple.cxf.muleinaction.com/\">" +
+            "<ns1:return>Hello, John</ns1:return></ns1:getGreetingResponse></soap:Body></soap:Envelope>";
 
 }
