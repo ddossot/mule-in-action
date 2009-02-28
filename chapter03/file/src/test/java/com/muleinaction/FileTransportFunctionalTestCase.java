a3074dd4355e9b975dc04223eda5f0a06ff0faf0
johndemic <johndemic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-02-28 18:29:19 +0000

    Thread.sleep instances replaced with a CountDownLatch.

--- chapter03/file/src/test/java/com/muleinaction/FileTransportFunctionalTestCase.java
+++ chapter03/file/src/test/java/com/muleinaction/FileTransportFunctionalTestCase.java
@@ -1,7 +1,10 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.context.notification.EndpointMessageNotificationListener;
+import org.mule.api.context.notification.ServerNotification;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.context.notification.EndpointMessageNotification;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 
@@ -10,6 +13,8 @@ import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
@@ -17,34 +22,46 @@ import java.util.UUID;
 public class FileTransportFunctionalTestCase extends FunctionalTestCase {
 
     private static String SOURCE_DIRECTORY = "./data/snapshot";
-       private static String DEST_DIRECTORY = "./data/archive";
+    private static String DEST_DIRECTORY = "./data/archive";
 
-       protected void doSetUp() throws Exception {
-           super.doSetUp();
-           FileUtils.cleanDirectory(new File(SOURCE_DIRECTORY));
-           FileUtils.cleanDirectory(new File(DEST_DIRECTORY));
-       }
+    private CountDownLatch latch = new CountDownLatch(1);
 
-       @Override
-       protected String getConfigResources() {
-           return "conf/file-xml-filter-config.xml";
-       }
+    protected void doSetUp() throws Exception {
+        super.doSetUp();
+        FileUtils.cleanDirectory(new File(SOURCE_DIRECTORY));
+        FileUtils.cleanDirectory(new File(DEST_DIRECTORY));
+        muleContext.registerListener(new EndpointMessageNotificationListener() {
+            public void onNotification(final ServerNotification notification) {
+                if ("fileService".equals(notification.getResourceIdentifier())) {
+                    final EndpointMessageNotification messageNotification = (EndpointMessageNotification) notification;
+                    if (messageNotification.getEndpoint().getName().equals("endpoint.file.data.archive")) {
+                        latch.countDown();
+                    }
+                }
+            }
+        });
+    }
 
-       public void testCorrectlyInitialized() throws Exception {
-           final Service service = muleContext.getRegistry().lookupService(
-                   "fileService");
-           assertNotNull(service);
-           assertEquals("fileModel", service.getModel().getName());
-       }
+    @Override
+    protected String getConfigResources() {
+        return "conf/file-xml-filter-config.xml";
+    }
 
-       public void testFileIsArchived() throws IOException, InterruptedException {
-           String filename = "SNAPSHOT-" + UUID.randomUUID().toString() + ".xml";
-           BufferedWriter out = new BufferedWriter(new FileWriter(SOURCE_DIRECTORY + "/" + filename));
-           out.write("data");
-           out.close();
-           Thread.sleep(2000);
-           assertEquals(1,FileUtils.listFiles(new File(DEST_DIRECTORY),new WildcardFileFilter("*.*"), null).size());
-       }
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "fileService");
+        assertNotNull(service);
+        assertEquals("fileModel", service.getModel().getName());
+    }
+
+    public void testFileIsArchived() throws IOException, InterruptedException {
+        String filename = "SNAPSHOT-" + UUID.randomUUID().toString() + ".xml";
+        BufferedWriter out = new BufferedWriter(new FileWriter(SOURCE_DIRECTORY + "/" + filename));
+        out.write("data");
+        out.close();
+        assertTrue("Message did not reach directory on time", latch.await(15, TimeUnit.SECONDS));
+        assertEquals(1, FileUtils.listFiles(new File(DEST_DIRECTORY), new WildcardFileFilter("*.*"), null).size());
+    }
 
 
 }
--- chapter03/file/src/test/java/com/muleinaction/FileXmlFilterTransportFunctionalTestCase.java
+++ chapter03/file/src/test/java/com/muleinaction/FileXmlFilterTransportFunctionalTestCase.java
@@ -1,7 +1,10 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.context.notification.EndpointMessageNotificationListener;
+import org.mule.api.context.notification.ServerNotification;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.context.notification.EndpointMessageNotification;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 
@@ -10,6 +13,8 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
@@ -19,10 +24,22 @@ public class FileXmlFilterTransportFunctionalTestCase extends FunctionalTestCase
     private static String SOURCE_DIRECTORY = "./data/snapshot";
     private static String DEST_DIRECTORY = "./data/archive";
 
+    private CountDownLatch latch = new CountDownLatch(1);
+
     protected void doSetUp() throws Exception {
         super.doSetUp();
         FileUtils.cleanDirectory(new File(SOURCE_DIRECTORY));
         FileUtils.cleanDirectory(new File(DEST_DIRECTORY));
+        muleContext.registerListener(new EndpointMessageNotificationListener() {
+            public void onNotification(final ServerNotification notification) {
+                if ("fileService".equals(notification.getResourceIdentifier())) {
+                    final EndpointMessageNotification messageNotification = (EndpointMessageNotification) notification;
+                    if (messageNotification.getEndpoint().getName().equals("endpoint.file.data.archive")) {
+                        latch.countDown();
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -42,7 +59,7 @@ public class FileXmlFilterTransportFunctionalTestCase extends FunctionalTestCase
         BufferedWriter out = new BufferedWriter(new FileWriter(SOURCE_DIRECTORY + "/" + filename));
         out.write("data");
         out.close();
-        Thread.sleep(2000);
+        assertTrue("Message did not reach directory on time", latch.await(15, TimeUnit.SECONDS));
         assertEquals(1,FileUtils.listFiles(new File(DEST_DIRECTORY),new WildcardFileFilter("*.xml"), null).size());
     }
 
