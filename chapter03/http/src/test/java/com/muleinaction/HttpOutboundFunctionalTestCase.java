4874ae83bbcacfb75f20847b52de638644c7a0f2
johndemic <johndemic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-02-28 18:37:22 +0000

    Replaced instances of Thread.sleep with a CountDownLatch

--- chapter03/http/src/test/java/com/muleinaction/HttpOutboundFunctionalTestCase.java
+++ chapter03/http/src/test/java/com/muleinaction/HttpOutboundFunctionalTestCase.java
@@ -2,12 +2,17 @@ package com.muleinaction;
 
 import org.mule.api.service.Service;
 import org.mule.api.MuleMessage;
+import org.mule.api.context.notification.EndpointMessageNotificationListener;
+import org.mule.api.context.notification.ServerNotification;
 import org.mule.tck.FunctionalTestCase;
 import org.mule.module.client.MuleClient;
+import org.mule.context.notification.EndpointMessageNotification;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 
 import java.io.File;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
@@ -15,6 +20,8 @@ import java.io.File;
 public class HttpOutboundFunctionalTestCase extends FunctionalTestCase {
 
     private static String DEST_DIRECTORY = "./data/reports";
+
+    private CountDownLatch latch = new CountDownLatch(1);
 
     @Override
     protected String getConfigResources() {
@@ -24,6 +31,16 @@ public class HttpOutboundFunctionalTestCase extends FunctionalTestCase {
     protected void doSetUp() throws Exception {
         super.doSetUp();
         FileUtils.cleanDirectory(new File(DEST_DIRECTORY));
+        muleContext.registerListener(new EndpointMessageNotificationListener() {
+            public void onNotification(final ServerNotification notification) {
+                if ("httpOutboundService".equals(notification.getResourceIdentifier())) {
+                    final EndpointMessageNotification messageNotification = (EndpointMessageNotification) notification;
+                    if (messageNotification.getEndpoint().getName().equals("endpoint.http.localhost.9765.backup.reports")) {
+                        latch.countDown();
+                    }
+                }
+            }
+        });
     }
 
     public void testCorrectlyInitialized() throws Exception {
@@ -35,12 +52,10 @@ public class HttpOutboundFunctionalTestCase extends FunctionalTestCase {
     }
 
     public void testMessageSentAndReceived() throws Exception {
-        Thread.sleep(5000);
+        assertTrue("Message did not reach directory on time", latch.await(15, TimeUnit.SECONDS));
         assertEquals(1, FileUtils.listFiles(new File(DEST_DIRECTORY), new WildcardFileFilter("*.xml"), null).size());
         File file = (File) FileUtils.listFiles(new File(DEST_DIRECTORY), new WildcardFileFilter("*.xml"), null).toArray()[0];
-       
-        assertEquals(XML,FileUtils.readFileToString(file));
-
+        assertEquals(XML, FileUtils.readFileToString(file));
     }
 
     private static String XML = "<backup><host>esb01</host></backup>\n";
--- chapter03/http/src/test/java/com/muleinaction/HttpPollingFunctionalTestCase.java
+++ chapter03/http/src/test/java/com/muleinaction/HttpPollingFunctionalTestCase.java
@@ -1,11 +1,16 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.context.notification.EndpointMessageNotificationListener;
+import org.mule.api.context.notification.ServerNotification;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.context.notification.EndpointMessageNotification;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 
 import java.io.File;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
@@ -14,9 +19,21 @@ public class HttpPollingFunctionalTestCase extends FunctionalTestCase {
 
     private static String DEST_DIRECTORY = "./data/polling";
 
+    private CountDownLatch latch = new CountDownLatch(1);
+
     protected void doSetUp() throws Exception {
         super.doSetUp();
         FileUtils.cleanDirectory(new File(DEST_DIRECTORY));
+         muleContext.registerListener(new EndpointMessageNotificationListener() {
+            public void onNotification(final ServerNotification notification) {
+                if ("httpPollingService".equals(notification.getResourceIdentifier())) {
+                    final EndpointMessageNotification messageNotification = (EndpointMessageNotification) notification;
+                    if (messageNotification.getEndpoint().getName().equals("endpoint.file.data.polling")) {
+                        latch.countDown();
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -30,7 +47,7 @@ public class HttpPollingFunctionalTestCase extends FunctionalTestCase {
 
         assertNotNull(service);
         assertEquals("httpPollingModel", service.getModel().getName());
-        Thread.sleep(5000);
+        assertTrue("Message did not reach directory on time", latch.await(15, TimeUnit.SECONDS));
         assertEquals(1, FileUtils.listFiles(new File(DEST_DIRECTORY), new WildcardFileFilter("*.*"), null).size());
     }
 
