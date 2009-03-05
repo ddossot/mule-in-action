25fddf1ab75eea7f1500af32889380382ee8437f
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-03-05 21:26:39 +0000

    migrated to Mule 2.2.0.

--- chapter09/security-filter/src/test/java/com/muleinaction/JaasPlainFunctionalTestCase.java
+++ chapter09/security-filter/src/test/java/com/muleinaction/JaasPlainFunctionalTestCase.java
@@ -1,24 +1,56 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.context.notification.ComponentMessageNotificationListener;
+import org.mule.api.context.notification.ServerNotification;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class JaasPlainFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/jaas-plain-jms-config.xml";
-	}
+    private CountDownLatch latch = new CountDownLatch(2);
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"jaasPlainService");
+    @Override
+    protected String getConfigResources() {
+        return "conf/jaas-plain-jms-config.xml";
+    }
 
-		assertNotNull(service);
-		assertEquals("jaasPlainModel", service.getModel().getName());
-	}
+    protected void doSetUp() throws Exception {
+        super.doSetUp();
+        muleContext.registerListener(new ComponentMessageNotificationListener() {
+            public void onNotification(final ServerNotification notification) {
+                if ("jaasPlainService".equals(notification.getResourceIdentifier())) {
+                    latch.countDown();
+                }
+            }
+        });
+    }
 
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "jaasPlainService");
+        assertNotNull(service);
+        assertEquals("jaasPlainModel", service.getModel().getName());
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public void testEndpointAuthenticated() throws Exception {
+        MuleClient client = new MuleClient(muleContext);
+
+        Map messageProperties = new HashMap();
+        messageProperties.put("MULE_USER", "Plain john::password");
+
+        client.send("jms://messages", TEST_PAYLOAD, messageProperties);
+        assertTrue("Message did not reach component on time", latch.await(15, TimeUnit.SECONDS));
+    }
+
+    private static String TEST_PAYLOAD = "TEST";
 }
