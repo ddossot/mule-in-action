546d8109b7d13e38d334b02038131b7cb752410f
john.demic <john.demic@gmail.com>
2009-03-06 00:36:52 +0000

    Migrated to Mule 2.2.0.  Functional test cases improved.

--- chapter09/security-manager/src/test/java/com/muleinaction/SecurityManagerLdapFunctionalTestCase.java
+++ chapter09/security-manager/src/test/java/com/muleinaction/SecurityManagerLdapFunctionalTestCase.java
@@ -1,19 +1,36 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @@author John D'Emic (john.demic@@gmail.com)
  */
 public class SecurityManagerLdapFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/security-manager-ldap-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/security-manager-ldap-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-	}
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "securityManagerHttpLdapService");
+        assertNotNull(service);
+        assertEquals("securityManagerHttpLdapModel", service.getModel().getName());
+    }
+
+    public void testEndpointAuthenticated() throws Exception {
+           MuleClient client = new MuleClient(muleContext);
+
+           MuleMessage message = client.send("http://john:password@localhost:8081/secure", TEST_PAYLOAD, null);
+           assertNotNull(message);
+           assertEquals(TEST_PAYLOAD, message.getPayloadAsString());
+       }
+
+       private static String TEST_PAYLOAD = "TEST";
+    
 
 }
