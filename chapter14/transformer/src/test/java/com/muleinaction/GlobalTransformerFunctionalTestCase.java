ca3aa440f39dbb3a9a27b3e01fc2eb403b4c441d
johndemic <johndemic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-03-07 03:39:25 +0000

    Migrated to Mule 2.2.0.  Functional test cases improved.

--- chapter14/transformer/src/test/java/com/muleinaction/GlobalTransformerFunctionalTestCase.java
+++ chapter14/transformer/src/test/java/com/muleinaction/GlobalTransformerFunctionalTestCase.java
@@ -1,24 +1,32 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class GlobalTransformerFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/global-transformer-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/global-transformer-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"groovyTransformerService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "groovyTransformerService");
+        assertNotNull(service);
+        assertEquals("groovyTransformerModel", service.getModel().getName());
+    }
 
-		assertNotNull(service);
-		assertEquals("groovyTransformerModel", service.getModel().getName());
-	}
+    public void messageTransformed() throws Exception{
+        MuleClient client = new MuleClient(muleContext);
+        client.sendAsync("vm://in", "hello, world.", null);
+        MuleMessage message = client.request("vm://out", 15000);
+        assertEquals("HELLO, WORLD.", message.getPayloadAsString());
+    }
 
 }
--- chapter14/transformer/src/test/java/com/muleinaction/GroovyTransformerFunctionalTestCase.java
+++ chapter14/transformer/src/test/java/com/muleinaction/GroovyTransformerFunctionalTestCase.java
@@ -1,7 +1,9 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
@@ -20,5 +22,12 @@ public class GroovyTransformerFunctionalTestCase extends FunctionalTestCase {
 		assertNotNull(service);
 		assertEquals("groovyTransformerModel", service.getModel().getName());
 	}
+
+    public void testMessageTransformed() throws Exception {
+        MuleClient client = new MuleClient(muleContext);
+        client.sendAsync("vm://in","hello, world.",null);
+        MuleMessage message = client.request("vm://out",15000);
+        assertEquals("HELLO, WORLD.", message.getPayloadAsString());
+    }
 
 }
