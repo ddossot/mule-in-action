6c7be842d8b71fa22dc45925fe1447916495c26c
johndemic <johndemic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-03-07 17:42:50 +0000

    CountDownLatch added to HttpInboundFunctionalTestCase

--- chapter03/http/src/test/java/com/muleinaction/HttpInboundFunctionalTestCase.java
+++ chapter03/http/src/test/java/com/muleinaction/HttpInboundFunctionalTestCase.java
@@ -2,12 +2,17 @@ package com.muleinaction;
 
 import org.mule.api.service.Service;
 import org.mule.api.MuleMessage;
+import org.mule.api.context.notification.EndpointMessageNotificationListener;
+import org.mule.api.context.notification.ServerNotification;
 import org.mule.tck.FunctionalTestCase;
 import org.mule.module.client.MuleClient;
+import org.mule.context.notification.EndpointMessageNotification;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 
 import java.io.File;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
@@ -15,6 +20,8 @@ import java.io.File;
 public class HttpInboundFunctionalTestCase extends FunctionalTestCase {
 
     private static String DEST_DIRECTORY = "./data/reports";
+
+    private CountDownLatch latch = new CountDownLatch(1);
 
     @Override
     protected String getConfigResources() {
@@ -24,6 +31,16 @@ public class HttpInboundFunctionalTestCase extends FunctionalTestCase {
     protected void doSetUp() throws Exception {
         super.doSetUp();
         FileUtils.cleanDirectory(new File(DEST_DIRECTORY));
+          muleContext.registerListener(new EndpointMessageNotificationListener() {
+            public void onNotification(final ServerNotification notification) {
+                if ("httpInboundService".equals(notification.getResourceIdentifier())) {
+                    final EndpointMessageNotification messageNotification = (EndpointMessageNotification) notification;
+                    if (messageNotification.getEndpoint().getName().equals("endpoint.file.data.reports")) {
+                        latch.countDown();
+                    }
+                }
+            }
+        });
     }
 
     public void testCorrectlyInitialized() throws Exception {
@@ -37,6 +54,7 @@ public class HttpInboundFunctionalTestCase extends FunctionalTestCase {
         MuleClient muleClient = new MuleClient(muleContext);
         MuleMessage response = muleClient.send("http://localhost:9765/backup-reports",
                 REPORT_XML, null);
+        assertTrue("Message did not reach directory on time", latch.await(15, TimeUnit.SECONDS));        
         assertEquals(1, FileUtils.listFiles(new File(DEST_DIRECTORY), new WildcardFileFilter("*.xml"), null).size());
         File file = (File) FileUtils.listFiles(new File(DEST_DIRECTORY), new WildcardFileFilter("*.xml"), null).toArray()[0];
         assertEquals(REPORT_XML, FileUtils.readFileToString(file));
