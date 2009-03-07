753e178255f469053c54f288fd793d78695d9e3a
johndemic <johndemic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-03-07 23:54:36 +0000

    improved functional test cases

--- chapter08/exception-strategy/src/test/java/com/muleinaction/DefaultServiceExceptionStrategyFunctionalTestCase.java
+++ chapter08/exception-strategy/src/test/java/com/muleinaction/DefaultServiceExceptionStrategyFunctionalTestCase.java
@@ -1,7 +1,9 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
@@ -19,6 +21,22 @@ public class DefaultServiceExceptionStrategyFunctionalTestCase extends Functiona
 
 		assertNotNull(service);
 		assertEquals("defaultServiceExceptionStrategyModel", service.getModel().getName());
-	}
+
+    }
+
+    public void testExceptionThrownToRIGAndRouted() throws Exception {
+        MuleClient client = new MuleClient(muleContext);
+        client.send("vm://RIG.In","test",null);
+        MuleMessage message = client.request("jms://error",15000);
+        assertNotNull(message);
+    }
+
+
+    public void testExceptionThrownToSRIGAndRouted() throws Exception {
+        MuleClient client = new MuleClient(muleContext);
+        client.send("vm://SRIG.In","test",null);
+        MuleMessage message = client.request("jms://error",15000);
+        assertNotNull(message);
+    }
 
 }
