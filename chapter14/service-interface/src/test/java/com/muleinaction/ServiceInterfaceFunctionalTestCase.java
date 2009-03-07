faff69fff4d554236c8b42d8c7a1ce84238f3b0e
johndemic <johndemic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-03-07 03:16:18 +0000

    Migrated to Mule 2.2.0.  Functional test cases improved.

--- chapter14/service-interface/src/test/java/com/muleinaction/ServiceInterfaceFunctionalTestCase.java
+++ chapter14/service-interface/src/test/java/com/muleinaction/ServiceInterfaceFunctionalTestCase.java
@@ -1,24 +1,40 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class ServiceInterfaceFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/service-interface-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/service-interface-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"javaInterfaceBindingService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "javaInterfaceBindingService");
 
-		assertNotNull(service);
-		assertEquals("javaInterfaceBindingModel", service.getModel().getName());
-	}
+        assertNotNull(service);
+        assertEquals("javaInterfaceBindingModel", service.getModel().getName());
+    }
+
+    public void testHashGenerated() throws Exception {
+        MuleClient client = new MuleClient(muleContext);
+        File inputFile = File.createTempFile("tmp", ".txt");
+        inputFile.deleteOnExit();
+        FileUtils.writeStringToFile(inputFile, "test");
+
+        System.out.println(inputFile.getAbsolutePath());
+        MuleMessage message = client.send("vm://MSC.In", inputFile.getName(), null);
+        assertEquals("098f6bcd4621d373cade4e832627b4f6", message.getPayloadAsString());        
+    }
 
 }
