21a199bc56b79f83f5452479242e41c49cc7636e
johndemic <johndemic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-03-07 02:13:16 +0000

    migrated to Mule 2.2.0.

--- chapter14/component/src/test/java/com/muleinaction/SimpleRhinoComponentFunctionalTestCase.java
+++ chapter14/component/src/test/java/com/muleinaction/SimpleRhinoComponentFunctionalTestCase.java
@@ -1,24 +1,32 @@
 package com.muleinaction;
 
 import org.mule.api.service.Service;
+import org.mule.api.MuleMessage;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.module.client.MuleClient;
 
 /**
  * @author John D'Emic (john.demic@gmail.com)
  */
 public class SimpleRhinoComponentFunctionalTestCase extends FunctionalTestCase {
 
-	@Override
-	protected String getConfigResources() {
-		return "conf/simple-rhino-component-config.xml";
-	}
+    @Override
+    protected String getConfigResources() {
+        return "conf/simple-rhino-component-config.xml";
+    }
 
-	public void testCorrectlyInitialized() throws Exception {
-		final Service service = muleContext.getRegistry().lookupService(
-				"rhinoMessageEnrichmentService");
+    public void testCorrectlyInitialized() throws Exception {
+        final Service service = muleContext.getRegistry().lookupService(
+                "rhinoMessageEnrichmentService");
+        assertNotNull(service);
+        assertEquals("rhinoMessageEnrichmentModel", service.getModel().getName());
+    }
 
-		assertNotNull(service);
-		assertEquals("rhinoMessageEnrichmentModel", service.getModel().getName());
-	}
+    public void testMessageEnriched() throws Exception {
+        MuleClient client = new MuleClient(muleContext);
+        client.sendAsync("vm://in", "test", null);
+        MuleMessage message = client.request("vm://out", 15000L);
+        assertNotNull(message);
+    }
 
 }
