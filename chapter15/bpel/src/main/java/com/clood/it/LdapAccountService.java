36aab277143f9ba1d6b5e470c670a4aafe31fef6
john.demic <john.demic@00d47abd-7054-0410-80d8-c3362abcef66>
2009-03-08 02:34:28 +0000

    removed bpel examples and jbpm startup script

--- chapter15/bpel/src/main/java/com/clood/it/LdapAccountService.java
+++
@@ -1,9 +0,0 @@
-package com.clood.it;
-
-public class LdapAccountService {
-
-    public void provision(Account account) {
-        System.out.println("Creating LDAP account" + account);
-    }
-}
-
