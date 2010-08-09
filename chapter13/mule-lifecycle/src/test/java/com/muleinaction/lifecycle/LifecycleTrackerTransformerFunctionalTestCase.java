d0a2d52732916c39fde4f3218df639fec1b3b207
ddossot <ddossot@00d47abd-7054-0410-80d8-c3362abcef66>
2010-08-09 23:39:34 +0000

    switched to 3RC1-SNAP

--- chapter13/mule-lifecycle/src/test/java/com/muleinaction/lifecycle/LifecycleTrackerTransformerFunctionalTestCase.java
+++ chapter13/mule-lifecycle/src/test/java/com/muleinaction/lifecycle/LifecycleTrackerTransformerFunctionalTestCase.java
@@ -1,3 +1,4 @@
+
 package com.muleinaction.lifecycle;
 
 import static org.junit.Assert.assertEquals;
@@ -10,26 +11,25 @@ import org.mule.module.client.MuleClient;
 /**
  * @author David Dossot (david@dossot.net)
  */
-public class LifecycleTrackerTransformerFunctionalTestCase {
+public class LifecycleTrackerTransformerFunctionalTestCase
+{
 
     @Test
-    public void trackLifecycle() throws Exception {
-        final MuleClient muleClient = new MuleClient(
-                "conf/lifecycle-config.xml");
+    public void trackLifecycle() throws Exception
+    {
+        final MuleClient muleClient = new MuleClient("conf/lifecycle-config.xml");
 
         final MuleContext muleContext = muleClient.getMuleContext();
         muleContext.start();
 
-        final MuleMessage result = muleClient.send("vm://EchoService.In",
-                "foo", null);
+        final MuleMessage result = muleClient.send("vm://EchoService.In", "foo", null);
 
-        final LifecycleTrackerTransformer ltt = (LifecycleTrackerTransformer) result
-                .getPayload();
+        final LifecycleTrackerTransformer ltt = (LifecycleTrackerTransformer) result.getPayload();
 
         muleContext.dispose();
         muleClient.dispose();
 
-        assertEquals("[setProperty, setMuleContext, initialise]", ltt
-                .getTracker().toString());
+        assertEquals("[setProperty, setMuleContext, initialise, initialise, start, stop]", ltt.getTracker()
+            .toString());
     }
 }
